7dcf47e29e8630894d53a299d044440062d3afed
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-02-02 10:58:19 -0700

    new views for about, contacct, and experience endpoints

---
+++ backend.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var urlencoded = bodyParser.urlencoded({extended: false});
+
+var app = express();
+app.set('template-engine', 'ejs');
+app.use('/', express.static('static'));
+
+app.get('/', function(req, res){
+  console.log(`Request has been made by ${req.url}`);
+  res.render('main.ejs');
+});
+
+app.get('/contact', function(req, res){
+  console.log(`Request has been made by ${req.url}`);
+  res.render('contact.ejs');
+})
+
+app.get('/portfolio', function(req, res){
+  res.render('portfolio.ejs');
+})
+
+app.listen(3000);
+console.log('Now listening to post 3000');
